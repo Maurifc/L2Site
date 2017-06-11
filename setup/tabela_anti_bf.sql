fdd937eaa525ce7795fc6263a6c7808df2c5a779
Mauri <onfocusman@gmail.com>
2017-06-11 16:06:58 -0300

    Removido SQL para criacao de tabela (em duplicidade)

--- setup/tabela_anti_bf.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS anti_bruteforce;
-CREATE TABLE `anti_bruteforce` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `ip` VARCHAR(45) NOT NULL,
-  `login` VARCHAR(45) NOT NULL,
-  `horaBloqueio` DATETIME(0) NOT NULL,
-  PRIMARY KEY (`id`));
